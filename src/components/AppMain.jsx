fd1b2c25689ba06abba4bca031e39b94c2a7f7d0
MicheleFumi <michele.fumi96@gmail.com>
2024-11-12 15:37:04 +0100

    add html

---
+++ src/components/AppMain.jsx
@@ -0,0 +1,28 @@
+export default function AppMain() {
+
+    return (
+
+
+        <main>
+            <div className="container">
+                <div className="blog-card">
+                    <div className="blog-img">
+                        <div className="img-size">600 X 400</div>
+                    </div>
+                    <div className="blog-description">
+                        <h3>Titolo del post</h3>
+                        <div>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis cumque ipsam nihil, ipsa debitis reiciendis in facere, cum et numquam aliquid modi error.
+                            Iste aliquid eligendi nulla, fuga quis laboriosam!
+                        </div>
+                        <button className="btn">LEGGI DI PIU'</button>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </main>
+    )
+}
